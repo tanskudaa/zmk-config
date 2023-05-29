bd481380ab5871c9bc2295524388a95e81b363c3
Taneli Hongisto <taneli.hongisto@protonmail.com>
2023-05-29 17:34:11 +0300

    Clear .keymap to more readable

--- config/keys_fi.h
+++ config/keys_fi.h
@@ -10,48 +10,50 @@
 #include <dt-bindings/zmk/modifiers.h>
 #include <dt-bindings/zmk/keys.h>
 
-// Umlaut letters
-#define FI_A_RING LEFT_BRACKET
-#define FI_O_UMLAUT SEMICOLON
-#define FI_A_UMLAUT APOSTROPHE
+// Accented letters
+#define A_RNG LEFT_BRACKET // Å
+#define O_UML SEMICOLON    // Ö
+#define A_UML APOSTROPHE   // Ä
+
+// Basic punctuation
+#define F_COM COMMA  // , Comma
+#define F_DOT PERIOD // . Period
+#define F_DASH SLASH // - Hyphen/Minus
 
 // US symbols
-#define FI_EXCL LS(N1)               // Exclamation
-#define FI_AT RA(N2)                 // At sign
-#define FI_HASH LS(N3)               // Hash
-#define FI_DLLR RA(N4)               // Dollar
-#define FI_PRCNT LS(N5)              // Percent
-#define FI_CARET LS(RIGHT_BRACKET)   // Caret (Dead)
-#define FI_AMPS LS(N6)               // Ampersand
-#define FI_STAR LS(BACKSLASH)        // Asterisk/Star
-#define FI_LPAR LS(N8)               // Left parenthesis
-#define FI_RPAR LS(N9)               // Right parenthesis
-#define FI_EQUAL LS(N0)              // Equal
-#define FI_PLUS MINUS                // Plus
-#define FI_MINUS SLASH               // Minus
-#define FI_UNDER QUESTION            // Underscore
-#define FI_SLASH LS(7)               // Forward slash
-#define FI_QMARK LS(MINUS)           // Question mark
-#define FI_BSLH RA(MINU8S)           // Backslash
-#define FI_PIPE RA(NON_US_BACKSLASH) // Pipe
-#define FI_SEMI LS(COMMA)            // Semicolon
-#define FI_COLON LS(PERIOD)          // Colon
-#define FI_SQT BACKSLASH             // Single quote/Apostrophe
-#define FI_DQT LS(N2)                // Double quote
-#define FI_COMMA COMMA               // Comma
-#define FI_LT NON_US_BACKSLASH       // Less than
-#define FI_DOT PERIOD                // Period
-#define FI_GT LS(NON_US_BACKSLASH)   // Greater than
-#define FI_LBKT RA(N8)               // Left bracket
-#define FI_LBRC RA(N7)               // Left brace
-#define FI_RBKT RA(N9)               // Right bracket
-#define FI_RBRC RA(N0)               // Right brace
-#define FI_GRAVE LS(EQUAL)           // Grave accent (dead)
-#define FI_TILDE RA(RIGHT_BRAKCET)   // Tilde (dead)
+#define F_EXCL LS(N1)               // ! Exclamation
+#define F_AT RA(N2)                 // @ At sign
+#define F_HASH LS(N3)               // # Hash
+#define F_DLLR RA(N4)               // $ Dollar
+#define F_PRCNT LS(N5)              // % Percent
+#define F_CRET LS(RIGHT_BRACKET)    // ^ Caret (Dead)
+#define F_AMP LS(N6)                // & Ampersand
+#define F_STAR LS(BACKSLASH)        // * Asterisk/Star
+#define F_LPAR LS(N8)               // ( Left parenthesis
+#define F_RPAR LS(N9)               // ) Right parenthesis
+#define F_EQ LS(N0)                 // = Equal
+#define F_PLUS MINUS                // + Plus
+#define F_UNDR QUESTION             // _ Underscore
+#define F_SLSH LS(N7)               // / Forward slash
+#define F_QUES LS(MINUS)            // ? Question mark
+#define F_BSLH RA(MINUS)            // \ Backslash
+#define F_PIPE RA(NON_US_BACKSLASH) // | Pipe
+#define F_SEMI LS(COMMA)            // ; Semicolon
+#define F_COLN LS(PERIOD)           // : Colon
+#define F_SQT BACKSLASH             // ' Single quote/Apostrophe
+#define F_DQT LS(N2)                // " Double quote
+#define F_LT NON_US_BACKSLASH       // < Less than
+#define F_GT LS(NON_US_BACKSLASH)   // > Greater than
+#define F_LBKT RA(N8)               // [ Left bracket
+#define F_LBRC RA(N7)               // { Left brace
+#define F_RBKT RA(N9)               // ] Right bracket
+#define F_RBRC RA(N0)               // } Right brace
+#define F_GRVE LS(EQUAL)            // ` Grave accent (dead)
+#define F_TLDE RA(RIGHT_BRACKET)    // ~ Tilde (dead)
 
 // Additional symbols
-#define FI_POUND RA(N3)       // Pound
-#define FI_EURO RA(E)         // Euro
-#define FI_CURR LS(N4)        // Currency sign
-#define FI_ACUTE PLUS         // Acute accent(dead)
-#define FI_DIAC RIGHT_BRACKET // Umlaut (dead)
+#define F_POUN RA(N3)        // £ Pound
+#define F_EURO RA(E)         // € Euro
+#define F_CURR LS(N4)        // ¤ Currency sign
+#define F_ACUT PLUS          // ´ Acute accent(dead)
+#define F_DIAC RIGHT_BRACKET // ¨ Umlaut (dead)
